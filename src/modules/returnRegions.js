7dfe96962bab413578d336a4cf92fac61bed8dcf
Egor Levchenko <antytoto@gmail.com>
2020-04-20 10:48:01 +0300

    created some address divs

--- src/modules/returnRegions.js
+++ src/modules/returnRegions.js
@@ -17,28 +17,31 @@ function returnRegions () {
   const RegionBlock = document.querySelector(".section_regions-w-offices");
   const moreCityName = RegionBlock.querySelector(".city_name");
   const moreInfo = RegionBlock.querySelector(".more__info");
-
   moreCityName.textContent = addresses[0].city;
   moreInfo.innerHTML = "";
-  addresses.forEach(
-    function() {
+  addresses[0].addresses.forEach(
+    function newOffice () {
       const createOffice = document.createElement("div");
-      const createAddress = document.createElement("address");
-      const createTime = document.createElement("span");
       createOffice.classList.add("more__office");
       moreInfo.append(createOffice);
 
-      const moreOffice = document.querySelector(".more__office");
-      createAddress.classList.add("more__address");
-      createAddress.textContent = "adresss";
-      moreOffice.append(createAddress);
-
-      createTime.classList.add("more__time");
-      createTime.textContent = "time";
-      moreOffice.append(createTime);
+      const moreOffice = document.getElementsByClassName(".more__office");
+      moreOffice.forEach(
+        function newAddress () {
+          const createAddress = document.createElement("address");
+          const createTime = document.createElement("span");
+          createAddress.textContent = "адрес";
+          createAddress.classList.add("more__address");
+          moreOffice.append(createAddress);
+          createTime.textContent = "время";
+          createTime.classList.add("more__time");
+          moreOffice.append(createTime);
+        }
+      )
     }
-  )
-  console.log("")
+  );
 }
+
+
 
 export { returnRegions };
